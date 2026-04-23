a97304387f010d66b1b8bc5a41ced5796a03fd81
hy-ahn00 <ha00.yoon00@gmail.com>
2026-04-23 15:12:16 +0900

    Add scroll event listener to toggle 'flip' class

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("envelope loaded");
+window.addEventListener("scroll", () => {
+  const env = document.getElementById("env");
+
+  if (window.scrollY > 150) {
+    env.classList.add("flip");
+  } else {
+    env.classList.remove("flip");
+  }
+});
